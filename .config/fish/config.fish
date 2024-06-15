d079f14ea0ba4684fdcc2580a52288b6388f2e30
AnderCabrera <andercabrerac@gmail.com>
2024-06-15 00:46:13 -0600

    adding fish, alacritty, pycritty and neofetch

---
+++ .config/fish/config.fish
@@ -0,0 +1,19 @@
+starship init fish | source
+#set -gx PATH $PATH /opt/nvim/
+set -x DENO_INSTALL "$HOME/.deno"
+set -x PATH "$DENO_INSTALL/bin" $PATH
+
+# Alias
+alias ls="exa -l --icons"
+alias lsa="exa -al --icons"
+alias lst="exa -alT --icons"
+alias lsg="exa -al --icons --git"
+alias lsgt="exa -alT --icons --git"
+alias cat="batcat"
+alias dotfiles="git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME"
+
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+    neofetch
+end
+    
